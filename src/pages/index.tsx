6e204652701e2941ee445ff29949479b9326330a
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-06-24 18:49:43 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import { trpc } from "@/utils/trpc";
+
+const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "World!" }]);
+
+
+  return (
+    <>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        {/* <div className="text-2xl text-center">Which Pokémon is Rounder?</div> */}
+        {/* <div className="p-2" /> */}
+        {/* <div className="border rounded p-8 flex justify-between items-center max-w-2xl"> */}
+          {/* <div className="w-16 h-16 bg-red-200" /> */}
+          {/* <div className="p-8">Vs</div> */}
+          {/* <div className="w-16 h-16 bg-red-200" /> */}
+        {/* </div> */}
+
+        {getContent(data, isLoading)}
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
+
+function getContent(data:any, isLoading:boolean){
+  if (isLoading) return <div>Loading..</div>;
+  if (data) return <div className="text-4xl">{data.greeting}</div>;
+}
